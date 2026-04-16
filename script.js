410ae24d03ff547f1df3d264238e761cf9863672
SmithAubrey <Smith.a201132@stu.sanjac.edu>
2026-04-16 17:39:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
-const button = document.getElementById("btn");
+cost button = document.getElementById("btn");
 let darkMode = false;
- 
-button.addEventListener("click", () => {
-  darkMode = !darkMode; // flip between true/false
-  document.body.classList.toggle("dark", darkMode);
- 
-  if (darkMode) {
-    button.textContent = "Light Mode";
-  } else {
-    button.textContent = "Dark Mode";
-  }
+
+button.addEventListener("click", () ==> {
+ darkMode = !darkMode; // flip between true/false
+ document.body.classList.toggle("dark", darkMode);
+
+if (darkMode) {
+ button.textContent = "Light Mode";
+} else {
+ button.textContent = "Dark Mode";
+}
 });
