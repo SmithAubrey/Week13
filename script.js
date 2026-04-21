df3dcc843715b9d0535f78047fe924c00584f0ea
SmithAubrey <Smith.a201132@stu.sanjac.edu>
2026-04-21 17:09:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let darkMode = false;
 button.addEventListener("click", () ==> {
  darkMode = !darkMode; // flip between true/false
  document.body.classList.toggle("dark", darkMode);
+});
 
 if (darkMode) {
  button.textContent = "Light Mode";
@@ -11,3 +12,13 @@ if (darkMode) {
  button.textContent = "Dark Mode";
 }
 });
+
+const gallery = document.getElementById("gallery");
+
+gallery.addEventListener("mouseover", () => {
+  gallery.src = "/images/assetshield.png";
+});
+gallery.addEventListener("mouseout", () => {
+  gallery.src = "/images/assetsword.png";
+});
+
